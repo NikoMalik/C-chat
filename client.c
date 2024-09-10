8e8c81c84e5c574b87c3b53f65b6b55d792f8fdb
nyanrewd <nanrewd@gmail.com>
2024-09-10 20:33:14 +0300

    Place system/libc headers first in alphabetical order

--- client.c
+++ client.c
@@ -1,11 +1,11 @@
+#include <arpa/inet.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main()
 {
--- server.c
+++ server.c
@@ -1,11 +1,12 @@
+#include <arpa/inet.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "types.h"
 
 void error(char *msg)
